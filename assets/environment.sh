8b209f968a7f9474ded53924379caa3bc86a51a9
Husky Robot <husky@ttic.edu>
2018-12-05 17:04:55 -0600

    added ROS auto-source in env.sh

--- assets/environment.sh
+++ assets/environment.sh
@@ -10,3 +10,8 @@ export ROS_MASTER_URI="http://$ROS_MASTER_HOST:11311"
 
 # configure LCM URL
 export LCM_DEFAULT_URL="udpm://$LCM_IP:$LCM_PORT?ttl=$LCM_TTL"
+
+# auto-source third-party script (if any)
+if [ -f /code/devel/setup.bash ]; then
+    source /code/devel/setup.bash --extend
+fi
