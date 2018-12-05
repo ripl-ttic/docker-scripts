3c143b6bb60e38f20ca7c74b80851000b93ffe13
Husky Robot <husky@ttic.edu>
2018-12-05 17:03:28 -0600

    added custom LCM_IP LCM_TTL

--- assets/environment.sh
+++ assets/environment.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+echo $LCM_IP
+
+# source ROS
 source /opt/ros/$ROS_DISTRO/setup.bash
+
+# configure ROS URL
 export ROS_MASTER_URI="http://$ROS_MASTER_HOST:11311"
+
+# configure LCM URL
+export LCM_DEFAULT_URL="udpm://$LCM_IP:$LCM_PORT?ttl=$LCM_TTL"
