05e467f53716c6a0fe51a2c369b8a09680b6cae1
Andrea F. Daniele <andreadaniele@drive.ai>
2019-03-15 18:37:30 -0700

    added CUSTOM_SETUP_FILE

--- assets/environment.sh
+++ assets/environment.sh
@@ -11,6 +11,14 @@ export ROS_MASTER_URI="http://$ROS_MASTER_HOST:11311"
 # configure LCM URL
 export LCM_DEFAULT_URL="udpm://$LCM_IP:$LCM_PORT?ttl=$LCM_TTL"
 
+# source custom file
+if [ -n "$CUSTOM_SETUP_FILE" ]; then
+  if [ -f "$CUSTOM_SETUP_FILE" ]; then
+    echo "Sourcing custom file '$CUSTOM_SETUP_FILE'..."
+    source "$CUSTOM_SETUP_FILE" --extend
+  fi
+fi
+
 # auto-source third-party script (if any)
 if [ -f /code/devel/setup.bash ]; then
     source /code/devel/setup.bash --extend
